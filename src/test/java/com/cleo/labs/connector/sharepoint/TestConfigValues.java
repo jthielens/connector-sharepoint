b3edb6092a4d7c18b570b81eaa0cd25aa585171b
John Thielens <jthielens@cleo.com>
2018-04-16 13:23:45 -0400

    update test config values

---
+++ src/test/java/com/cleo/labs/connector/sharepoint/TestConfigValues.java
@@ -0,0 +1,12 @@
+package com.cleo.labs.connector.sharepoint;
+
+public class TestConfigValues {
+
+    public static final String URL = "https://company-my.sharepoint.com/personal/user_company_com";
+    public static final String USERNAME = "user@company.com";
+    public static final String PASSWORD = "password";
+
+    private TestConfigValues() {
+    }
+
+}
